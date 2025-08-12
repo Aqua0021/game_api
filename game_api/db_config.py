bb11bd431fcb78fc4d0de1c3ef905812f19852f9
Aqua0021 <rohit99073@gmail.com>
2025-08-12 22:25:30 +0530

    Switch to Neon PostgreSQL

--- game_api/db_config.py
+++ game_api/db_config.py
@@ -1,15 +1,10 @@
-import psycopg2
 import os
+import psycopg2
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
+# Load variables from .env file
 load_dotenv()
 
 def get_connection():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        sslmode="require"
-    )
+    conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+    return conn
